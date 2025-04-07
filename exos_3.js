80f8d87a38e853f5a43dd50a5e65c29de7315afd
NiaNia2 <lacodeuse@protonmail.com>
2025-04-07 14:19:45 +0200

    ajout exos_3

---
+++ exos_3.js
@@ -0,0 +1,15 @@
+let firstName = "Bob"
+let secondName = "Eponge"
+let stock
+
+stock = firstName;
+firstName = secondName;
+secondName = stock;
+
+console.log(firstName)
+console.log(secondName)
+
+
+
+
+
