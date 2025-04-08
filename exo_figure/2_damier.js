aa78d524b632691299171c2e09b8ef47e146fe16
NiaNia2 <lacodeuse@protonmail.com>
2025-04-08 15:07:35 +0200

    modif 2_damier

--- exo_figure/2_damier.js
+++ exo_figure/2_damier.js
@@ -3,11 +3,20 @@ let result = ''
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++)
         if (i % 2 == 0) {
-            result += '⬛' + '⬜'
+            if (j % 2 == 0) {
+                result += '⬛'
+            } else {
+                result += '⬜'
+            }
         } else {
-            result += '⬜' + '⬛'
+            if (j % 2 == 0) {
+                result += '⬜'
+            } else {
+                result += '⬛'
+            }
         }
     result += "\n"
 }
 
 console.log(result);
+
